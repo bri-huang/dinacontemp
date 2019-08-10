bfc3a33528a6caa26ccae1cff15436dc45c63f56
Brian Huang <huang.brian.c@gmail.com>
2019-08-10 13:10:50 -0500

    Update dinacontemp.ino

--- dinacontemp.ino
+++ dinacontemp.ino
@@ -52,15 +52,17 @@ float latestTempF;
 float minTemp = 80;
 float maxTemp = 90;
 
-/********************************************************************************/
+/*\\*******************************************************************************/
 void setup() {
   pinMode(5, OUTPUT);
   rht.begin(4);
   Wire.begin();
+  delay(100);
   lcd.begin(Wire); //Set up the LCD for I2C communication
 
+  delay(1);    //extra delays inserted b/c of funny timing issues.
   lcd.clear(); //Clear the display - this moves the cursor to home position as well
-
+  delay(1);
   lcd.print("Connecting to:  ");
   lcd.print(ssid);
 
@@ -71,10 +73,16 @@ void setup() {
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+  strip2.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip2.show();            // Turn OFF all pixels ASAP
+  strip2.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+
   colorWipe(strip.Color(255,   0,   0), 50); // Red
   colorWipe(strip.Color(0,   255,   0), 50); // Green
   colorWipe(strip.Color(0,   0,   255), 50); // Blue
   theaterChaseRainbow(50);
+  rainbow(10);
 }
 /********************************************************************************/
 void loop()
@@ -109,6 +117,7 @@ void loop()
     lcd.print(latestHumidity);
     lcd.print("%");
     Cayenne.loop();
+    rainbow(10); 
   }
 }
 
@@ -243,11 +252,35 @@ void clearLCD()
   Wire.endTransmission();
 }
 /******************************************************************/
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256) {
+    for (int i = 0; i < strip.numPixels(); i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      strip2.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip2.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
+/******************************************************************/
 void theaterChaseRainbow(int wait) {
   int firstPixelHue = 0;     // First pixel starts at red (hue 0)
   for (int a = 0; a < 30; a++) { // Repeat 30 times...
     for (int b = 0; b < 3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      strip2.clear();         //   Set all pixels in RAM to 0 (off)
       for (int c = b; c < strip.numPixels(); c += 3) {
         int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
         uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
